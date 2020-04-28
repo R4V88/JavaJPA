3b185b665acc556151a09d488e6a760944e8e9c9
R4v88 <sochacki.rfl@gmail.com>
2020-04-28 18:11:43 +0200

    ProjectDao Interface is ready

--- src/main/java/model/dao/ProjectDao.java
+++ src/main/java/model/dao/ProjectDao.java
@@ -1,4 +1,14 @@
 package model.dao;
 
+import model.Project;
+
+import java.util.List;
+
 public interface ProjectDao {
+
+    public List<Project> getAllProjects();
+
+    public Project getProjectById(long id);
+
+    public Project getProjectByTitle(String title);
 }
