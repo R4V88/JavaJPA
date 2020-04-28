ddd4010b0e0ebc14afd02801b378360061a724f6
R4v88 <sochacki.rfl@gmail.com>
2020-04-28 22:36:13 +0200

    UserDao Interface is ready

--- src/main/java/dao/NoteDao.java
+++ src/main/java/dao/NoteDao.java
@@ -1,4 +1,21 @@
 package dao;
 
+import model.Note;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public interface NoteDao {
+
+    void createNote (EntityManager em);
+
+    List<Note> findAllNotes(EntityManager em);
+
+    Note findNoteById(EntityManager em, Long id);
+
+    void updateNoteTitleById(EntityManager em, Long id);
+
+    void updateNoteTextFieldById(EntityManager em, Long id);
+
+    void removeNoteById(EntityManager em, Long id);
 }
