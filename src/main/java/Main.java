954e003fc341d34ed35a945292ab99c096ec1f83
R4v88 <sochacki.rfl@gmail.com>
2020-04-28 14:06:04 +0200

    UserDao Interface done

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,11 @@ import javax.persistence.Persistence;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("toDoListPu");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
 
-
-
-
         entityManager.close();
         entityManagerFactory.close();
-
     }
 }
--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -1,4 +1,26 @@
 package dao;
 
+import model.Project;
+import model.User;
+import model.enums.Role;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
 public interface UserDao {
+
+    List<User> findAllUsers(EntityManager em);
+
+    List<User> findUsersByRole(EntityManager em, Role role);
+
+    List<User> findUsersByProject(EntityManager em, String projectTitle);
+
+    User findUserById(EntityManager em, long id);
+
+    User findUserByLogin(EntityManager em, String name);
+
+    User findUserByEmail(EntityManager em, String email);
+
+    User findUserByNameAndSurname(EntityManager em, String name, String surname);
+
 }
